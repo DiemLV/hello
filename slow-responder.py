3419cf89f6acb195f9f1a71e949222f65e6c3ac6
diemlv <30806360+diemlv@users.noreply.github.com>
2025-08-12 23:23:27 +0700

    Update slow-responder.py

--- slow-responder.py
+++ slow-responder.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def slow():
     time.sleep(20)
-    return "Done after 20 seconds"
+    return "Done after 20 seconds\n=====================\n\n\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
